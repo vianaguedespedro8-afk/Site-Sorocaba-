59aed81d4a7b090b77c3cb8cca4e768f64629937
vianaguedespedro8-afk <vianaguedespedro8@gmail.com>
2026-09-25 19:41:39 -0300

    Update and rename Config.js to config.js

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 window.EVENT_CONFIG = {
 
-  // Mesmo Google Sheets / Apps Script usado no site da comunidade.
   LEADS_ENDPOINT:
     "https://script.google.com/macros/s/AKfycbxHBEKVaqv_oirQM-59fx7DJNroPUZQEzL09y6UOTHyT2-uEyVd7YSa2FpGX7ruQVdg/exec",
 
-  // Mesma comunidade usada no site principal.
   COMMUNITY_URL:
     "https://chat.whatsapp.com/J7hPvR3rNpIGvlUX9S9aYu",
 
   GTM_ID:
     "GTM-W65P6TBG",
 
+  META_PIXEL_ID:
+    "",
+
+  // abre o popup praticamente na hora
+  POPUP_DELAY_MS:
+    50,
+
+  // São Paulo já vem selecionado
+  UF_PADRAO:
+    "SP",
+
   EVENT: {
-
-    city:
-      "Sorocaba",
-
-    uf:
-      "SP",
-
-    region:
-      "sorocaba",
 
     firstStart:
       "2026-09-26T09:30:00-03:00",
@@ -30,9 +30,5 @@ window.EVENT_CONFIG = {
 
     end:
       "2026-09-26T18:00:00-03:00"
-  },
-
-  // Popup aparece praticamente assim que a página carrega.
-  POPUP_DELAY_MS:
-    350
+  }
 };
