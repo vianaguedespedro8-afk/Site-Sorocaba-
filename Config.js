cbe957e93925f4e2156c5ca814626db6ed9306e8
vianaguedespedro8-afk <vianaguedespedro8@gmail.com>
2026-09-25 19:01:34 -0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,38 @@
+window.EVENT_CONFIG = {
+
+  // Mesmo Google Sheets / Apps Script usado no site da comunidade.
+  LEADS_ENDPOINT:
+    "https://script.google.com/macros/s/AKfycbxHBEKVaqv_oirQM-59fx7DJNroPUZQEzL09y6UOTHyT2-uEyVd7YSa2FpGX7ruQVdg/exec",
+
+  // Mesma comunidade usada no site principal.
+  COMMUNITY_URL:
+    "https://chat.whatsapp.com/J7hPvR3rNpIGvlUX9S9aYu",
+
+  GTM_ID:
+    "GTM-W65P6TBG",
+
+  EVENT: {
+
+    city:
+      "Sorocaba",
+
+    uf:
+      "SP",
+
+    region:
+      "sorocaba",
+
+    firstStart:
+      "2026-09-26T09:30:00-03:00",
+
+    secondStart:
+      "2026-09-26T14:30:00-03:00",
+
+    end:
+      "2026-09-26T18:00:00-03:00"
+  },
+
+  // Popup aparece praticamente assim que a página carrega.
+  POPUP_DELAY_MS:
+    350
+};
